5101862027e7573df8599f36baaabe54dea5784f
mohammad-alshish <eng.moahammed.majed@gamil.com>
2022-07-27 13:21:40 +0300

    Done

--- snacks/models.py
+++ snacks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class Snack(models.Model):
     name = models.CharField(max_length=64)
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    description = models.CharField(max_length=200, default='Add you notes,please!')
+    description = models.TextField(max_length=200, default='Add you notes,please!')
 
     def __str__(self):
         return self.name
